e94e3590fab895066a1bdbdbf9932945ee0b94d1
ut-kr <utkarsh@cyberfear.com>
2022-03-29 09:26:55 +1100

    ✨ support for save & encryption

--- transmit/iscmd.m
+++ transmit/iscmd.m
@@ -1,9 +1,44 @@
 % looks at the head of a message to confirm if its a command
 function is_it = iscmd(cmd, text)
   is_it = false;
-  if length(text) > length(cmd)
-    if text(2:(length(cmd)+1)) == cmd
-      is_it = true;
-    end
+  pass_save = false;
+
+  switch cmd
+    case 'encrypt'
+      to_look = ['[', ']'];
+      to_avoid = ['{', '}'];
+      pass_save = true;
+
+    case 'save'
+      to_look = ['{', '}'];
+      to_avoid = ['[', ']'];
+      pass_save = true;
+
+    otherwise
+      if length(text) > length(cmd)
+        if text(2:(length(cmd)+1)) == cmd
+          is_it = true;
+        end
+      end
   end
+
+  if pass_save
+      if text(2) == to_look(1, 1)
+        if ~isempty(find(text == to_look(1, 2), 1))
+            is_it = true;
+        end
+      
+      elseif text(2) == to_avoid(1, 1)
+        save_until = find(text == to_avoid(1, 2));
+        if ~isempty(save_until)
+          if text(save_until + 1) == to_look(1, 1)
+            if ~isempty(find(text == to_avoid(1, 2), 1))
+              is_it = true;
+            end
+          end
+        end
+
+      end 
+  end
+
 end
