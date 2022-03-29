31357211f0307d9b0069ab96cceefb0486a95c8d
ut-kr <utkarsh@cyberfear.com>
2022-03-30 07:50:51 +1100

    🎨 indentation fix

--- transmit/text_filter.m
+++ transmit/text_filter.m
@@ -2,15 +2,15 @@
 function [extra_info, message] = text_filter(expected, text)
   % extra info would be either password or file name
   if isequal(expected, 'encrypt')
-      borders = ['[', ']'];
+    borders = ['[', ']'];
   elseif isequal(expected, 'save')
-      borders = ['{', '}'];
+    borders = ['{', '}'];
   end
+
   starting = find(text == borders(1, 1));
   ending = find(text == borders(1, 2));
 
   extra_info = text(1, (starting+1):(ending-1) );
   message = text(1, (ending+1):end);
 
-  fprintf('%s\n%s\n', extra_info, message)
 end
