1547f8e33c10556ea633c738625893ba24e8af23
ut-kr <utkarsh@cyberfear.com>
2022-04-04 14:41:38 +1000

    🎁 saves passed data in a file

---
+++ recieve/save_sit.m
@@ -0,0 +1,11 @@
+% save passed content in a file 
+function save_sit(user, time, file_name, content)
+  file_name = [file_name, '.sit'];
+  c = clock;
+  date = [num2str(c(3)), '/', num2str(c(2)), '/', num2str(c(1))];
+
+  new_sit = fopen(file_name, 'w');
+  fprintf(new_sit, '{%s} on [%s] at [%s]\n%s', user, date, time, content);
+  fclose(new_sit);
+  
+end
