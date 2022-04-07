a042199eb87ce94dacce8f96d6723e8969866a47
ut-kr <utkarsh@cyberfear.com>
2022-04-07 16:19:14 +1000

    🔧 removed redundant conversion

--- transmit/send_signal.m
+++ transmit/send_signal.m
@@ -1,14 +1,14 @@
 function send_signal(str_bin)
-  num_bin = zeros(1, length(str_bin));
+  num_bin = zeros(length(str_bin), 1);
   for i = 1:length(str_bin)
-      num_bin(1, i) = str2num(str_bin(i));
+      num_bin(i, 1) = str2num(str_bin(i));
   end
 
   % Use daq.getDevices to check the device
   session = daq.createSession('ni');
   session.addAnalogOutputChannel('myDAQ1',0,'Voltage');
   session.Rate = 100; 
-  dataSeq = num_bin'; % column vector
+  dataSeq = num_bin; % column vector
   voltage = 5; % value to output port
   queueOutputData(session,dataSeq*voltage); % prepare data sequence for output
   session.startForeground; % output the queued date
