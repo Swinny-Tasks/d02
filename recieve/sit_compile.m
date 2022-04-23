b659a679f0a050aae17e3c9a78f3cee7379465ce
ut-kr <utkarsh@cyberfear.com>
2022-04-24 00:40:14 +1000

    🐛 one-digit time duration fix

--- recieve/sit_compile.m
+++ recieve/sit_compile.m
@@ -12,17 +12,20 @@ function sit_compile(code)
     on_LEDs = [0 0 0 0 0 0 0 0];
     
     % generating list of LEDs dealt with in this process
-    processes = string(zeros(length(content)/5, 2));
-    for j = 1:(length(content)/5)
-      processes(j, 1) = string(content((5*j-4):(5*j-3)));  % LED Name
-      processes(j, 2) = string(content(5*j-1));            % on time
+    before_time = find(content == '(');
+    after_time = find(content == ')');
+    processes = string(zeros(length(before_time), 2));
+    for j = 1:(length(before_time))
+      index = before_time(j);
+      processes(j, 1) = string(content((index-2):(index-1)));  % LED Name
+      processes(j, 2) = string(content((index+1):(after_time(j)-1))); % on time
     end
 
     % sorting LEDS with assending time order
     processes = sortrows(processes, 2); 
 
     % change 'time on' with 'time difference'
-    for k = (length(content)/5):-1:2
+    for k = length(processes):-1:2
       diff = str2double(processes(k, 2)) - str2double(processes(k-1, 2));
       processes(k, 2) = string(diff);
     end
@@ -35,7 +38,7 @@ function sit_compile(code)
       end
     end
 
-    %turning all reqested lights on
+    % turning all reqested lights on
     dev.outputSingleScan(on_LEDs);
     dev.release();
 
