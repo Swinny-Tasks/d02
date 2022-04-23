27f6703eb73dbacdb1eb5d5068b5b26a34a8c948
ut-kr <utkarsh@cyberfear.com>
2022-04-23 19:08:40 +1000

    ✨ understand input

---
+++ recieve/sit_compile.m
@@ -0,0 +1,14 @@
+function sit_compile(code)
+  % getting all one-time actions together
+  code = split(code, ";");
+  for i = 1:length(code)
+    content = char(code(i));
+
+    processes = string(zeros(length(content)/5, 2));
+    for j = 1:(length(content)/5)
+      processes(j, 1) = string(content((5*j-4):(5*j-3)));  % LED Name
+      processes(j, 2) = string(content(5*j-1));            % on time
+    end
+
+  end
+end
