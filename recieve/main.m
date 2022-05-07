b7d40ed73fea1ab9bbf556729c183aba92f9574e
ut-kr <utkarsh@cyberfear.com>
2022-05-07 12:51:25 +1000

    ✨ time sync

--- recieve/main.m
+++ recieve/main.m
@@ -12,27 +12,26 @@ username = 'user';
 s = daq('ni');
 s.addinput('myDAQ1',0, 'Voltage');
 message_bin = '';
-s.Rate = 2000;
+s.Rate = 5000;
 
 % program would continue working- waiting for text; without any limit
 while true
-  sensor_data = read(s, seconds(1), "OutputFormat","Matrix"); % collect sensor data
-  
-  raw_bin = erase(num2str([sensor_data > 2]'), ' ');
-  
-  if contains(raw_bin, "11101110111")   % look for preamble
-    if contains(raw_bin, "00011111110") % look for postamble
-      % complete messasge recorded
-      message = slice_msg(raw_bin);
+  if (rem(second(datetime('now')), 2) == 0)
+    fprintf("start  ")
 
-    else
-      % means the scan only recoded starting of the whole message
-      message_bin = raw_bin; 
-    
+    % collect sensor data
+    sensor_data = read(s, seconds(1.55), "OutputFormat","Matrix");
+  
+    raw_bin = erase(num2str([sensor_data > 2]'), ' ');
+  
+    if contains(raw_bin, "11101110111")   % look for preamble
+      if contains(raw_bin, "00011111110") % look for postamble
+        % complete messasge recorded
+        message = slice_msg(raw_bin);
+        username = decode(message, username);
+        disp(' ')
+      end
     end
-  else
-    continue;
+    disp("end")
   end
-  username = decode(message, username);
-  disp(' ')
 end
