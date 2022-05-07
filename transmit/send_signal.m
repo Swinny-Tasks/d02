0a99df637b8cfbbc5b638cfd8410c3a0c2bddb6b
ut-kr <utkarsh@cyberfear.com>
2022-05-07 12:51:07 +1000

    ✨ time sync

--- transmit/send_signal.m
+++ transmit/send_signal.m
@@ -7,11 +7,26 @@ function send_signal(str_bin)
   % Use daq.getDevices to check the device
   session = daq.createSession('ni');
   session.addAnalogOutputChannel('myDAQ1',0,'Voltage');
-  session.Rate = 2000; 
+  session.Rate = 5000; 
   dataSeq = num_bin; % column vector
   voltage = 5; % value to output port
   queueOutputData(session,dataSeq*voltage); % prepare data sequence for output
-  session.startForeground; % output the queued date
-  session.release();
+  
+  while (true)
+    t1 = datetime('now');
+    
+    if (rem(second(t1), 2) == 0)
+      pause(0.5); 
+      session.startForeground; % output the queued date
+      session.release();
+      t2 = datetime('now');
+      
+      if (seconds(diff(datetime([t1;t2]))) > 1.5)
+        fprintf(2, "message too big\n");
+      end
+      
+      break;
+    end
+  end
 
 end
