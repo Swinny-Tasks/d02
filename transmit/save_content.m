304c8466dbae7bf9ff9c6a3a6095370d28c9d7ee
ut-kr <utkarsh@cyberfear.com>
2022-05-08 21:56:16 +1000

    🐛 fix save date format

--- transmit/save_content.m
+++ transmit/save_content.m
@@ -1,11 +1,8 @@
 % save passed content in a file 
-function save_content(user, time, file_name, content, extension)
-  file_name = [file_name, extension];
-  c = clock;
-  date = [num2str(c(3)), '/', num2str(c(2)), '/', num2str(c(1))];
+function save_content(user, time, file_name, content)
+  file_name = ['files/', file_name];
 
   new_sit = fopen(file_name, 'w');
-  fprintf(new_sit, '{%s} on [%s] at [%s]\n%s', user, date, time, content);
+  fprintf(new_sit, '{%s} on [%s]\n%s', user, time, content);
   fclose(new_sit);
-  
 end
