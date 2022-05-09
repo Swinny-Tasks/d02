824d4e6cd86025522a0fe4d598410b4c8b7b7efb
ut-kr <utkarsh@cyberfear.com>
2022-05-09 20:30:17 +1000

    🐛 save time fix final

--- recieve/save_txt.m
+++ recieve/save_txt.m
@@ -1,8 +1,6 @@
 % save passed content in a file 
-function save_txt(user, file_name, content)
+function save_txt(user, time, file_name, content)
   file_name = [file_name, '.sit'];
-  c = clock;
-  date = [num2str(c(3)), '/', num2str(c(2)), '/', num2str(c(1))];
 
   new_sit = fopen(file_name, 'w');
   fprintf(new_sit, '{%s} on [%s] at [%s]\n%s\n', user, date, time, content);
