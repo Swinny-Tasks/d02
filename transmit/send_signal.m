ca759ff31b6f7826ae1f958797c592faa0f2f287
ut-kr <utkarsh@cyberfear.com>
2022-05-09 13:43:24 +1000

    🔧 cleaned & optimmised send algorithm

--- transmit/send_signal.m
+++ transmit/send_signal.m
@@ -1,9 +1,10 @@
 function send_signal(str_bin)
   num_bin = zeros(length(str_bin), 1);
+  
+  % convert message binary string to numerical array
   for i = 1:length(str_bin)
-      num_bin(i, 1) = str2num(str_bin(i));
+      num_bin(i, 1) = str2double(str_bin(i));
   end
-
 
   % Use daq.getDevices to check the device
   session = daq.createSession('ni');
@@ -13,20 +14,19 @@ function send_signal(str_bin)
   voltage = 5; % value to output port
   queueOutputData(session,dataSeq*voltage); % prepare data sequence for output
 
-
+  % send message only on even-clock-edge & if message size is reasonable
   while (true)
-    t1 = datetime('now');
-    
-    if (rem(second(t1), 2) == 0)
-      session.startForeground; % output the queued date
-      session.release();
-      t2 = datetime('now');
-      
-      if (seconds(diff(datetime([t1;t2]))) > 1) 
+    % if the data could be transmitted under a second
+    if (length(str_bin) > session.Rate)
         fprintf(2, "message too big\n");
+        break;
+    else
+      % if time is exactly event; i.e. including nano/mili seconds
+      if (rem(second(datetime('now')), 2) == 0)
+        session.startForeground; % output the queued date
+        session.release();
+        break;
       end
-      
-      break;
     end
   end
 
