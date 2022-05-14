a5ee1b446d20306f83b01a8c004496ac58209ba8
ut-kr <utkarsh@cyberfear.com>
2022-05-15 02:49:57 +1000

    ✨ auto-environment adjustment added

--- recieve/main.m
+++ recieve/main.m
@@ -11,7 +11,7 @@ username = 'user';
 s = daq('ni');
 s.addinput('myDAQ1',0, 'Voltage');
 message_bin = '';
-s.Rate = 256;
+s.Rate = 12000;
 prev_time = -1; % useful to prevent multiple call in a single time frame
 
 % program would continue working- waiting for text; without any limit
@@ -20,22 +20,22 @@ while true
   if ((rem(t, 2) == 0) && (t ~= prev_time))
     prev_time = t;
 
-    % env_level = read(s, seconds(0.1), "OutputFormat","Matrix");
-    % env_level = max(env_level); % max value of 0, i.e. environment level
-    env_level = 1;
-
-
     % collect sensor data
     sensor_data = read(s, seconds(1.55), "OutputFormat","Matrix");
-
-    raw_bin = erase(num2str([sensor_data > env_level]'), ' ');
-
-    if contains(raw_bin, "11101110111")   % look for preamble
-      if contains(raw_bin, "00011111110") % look for postamble
-        % complete messasge recorded
-        message = slice_msg(raw_bin);
-        username = decode(message, username);
-      end
+    smooth_data = zeros(1, 186);
+    
+    for i = 1:186
+      smooth_data(i) = median(sensor_data((((i*100)-99)) : (i*100)));
     end
+
+    env_level = (max(smooth_data) + min(smooth_data))/2;
+    raw_bin = num2str([smooth_data > env_level]')';
+
+    message = slice_msg(raw_bin);
+    if ~isempty(message)
+      username = decode(message, username);
+    end
+
+
   end
 end
